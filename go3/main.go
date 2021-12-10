ecc9ddff40638bc424f1ce5ac489a24b09c17ed2
foreverlz1111 <45527640+foreverlz1111@users.noreply.github.com>
2021-12-11 00:06:56 +0800

    更新文件夹

--- go3/main.go
+++ go3/main.go
@@ -3,6 +3,8 @@ import(
 	"fmt"
 	"math/rand"
 	"time"
+	"strings"
+	
 )
 type vertex struct{
      X int
@@ -16,6 +18,10 @@ var(
      )
 var array1 [8]int
 var str1 [2]string
+
+type mapping struct{
+     Lat,Long float64
+     }
 
 func main(){
      fmt.Println("hello pointer")
@@ -88,7 +94,61 @@ func main(){
       if  array_emp == nil {
       	 fmt.Println("空数组！")
 	 }
+
+      array6 := make([]int ,5)//make 函数分配一个元素为零的数组
+      fmt.Printf("array6 ")
+      printSlice(array6)
+
+      array7 := make([]int , 0 , 5)//make(array,len,cap)
+      fmt.Printf("array7 ")
+      printSlice(array7)
+
+      array8 := array7[:2]
+      fmt.Printf("array8 ")
+      printSlice(array8)
+
+      array9 := array8[2:5]
+      fmt.Printf("array9 ")
+      printSlice(array9)
+
+      array10 := [][]string{
+      	      make([]string,3),
+	      make([]string,3),
+      	      make([]string,3),
+      }
+      for i := 0 ;i < len(array10);i++{
+      	  for j := 0;j < len(array10);j++{
+	      if i == 0{
+	      	 array10[i][j] = "Y"
+		 }else{
+		 array10[i][j] = "K"
+		 }
+	    }
+	 }
+      
+      for i := 0 ;i < len(array10);i++{
+      	  for j := 0;j < len(array10);j++{
+	      fmt.Printf("%s \n",strings.Join(array10[i]," "))
+	      }
+	    }
+       array11 := []int{2,4,5,7,3,1,4}
+       for i,v := range array11{
+       	   fmt.Printf("index(%d) = %d \n",i,v)//下标+下标对应的值
+	   }
+       array12 := make([]int,10)
+       for i := range array12{
+       	   array12[i] = 1 << uint(i)// 2^i
+	   }
+       for _,value := range array12{
+       //赋予下标 _ 进行忽略
+       	   fmt.Printf("array12[] = %d \n",value)
+	   }
+	   map1 := make(map[string]mapping)//必须用make初始化后使用
+	   map1["str111"] = mapping{
+	   	      101010,1111111,
+		      }
+	   fmt.Println("map1 映射:",map1)
 }
 func printSlice(s []int){
-     fmt.Printf("长度= %d ，首个元素 = %d s= %v\n",len(s),cap(s),s)
+     fmt.Printf("长度= %d ，首个元素 = %d \"array\"= %v\n",len(s),cap(s),s)
 }
