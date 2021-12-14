fbf4b67c70f531f0c4dea0dd1bfb9cb08b99c255
foreverlz1111 <45527640+foreverlz1111@users.noreply.github.com>
2021-12-15 00:34:03 +0800

    新增内容

--- go4/main.go
+++ go4/main.go
@@ -5,13 +5,29 @@ import(
 	"math"
 	
 )
-type vertex struct{
+type vertex1 struct{
 	X, Y float64
 }
-func (v vertex)Abs()float64{
+type myfloat float64
+func (v vertex1)abs()float64{
 	return math.Sqrt(v.X * v.X + v.Y * v.Y)
 }
+func abs(v vertex1)float64{
+	return math.Sqrt(v.X * v.X + v.Y * v.Y)
+}
+func (myf myfloat)abs()float64{
+	if myf < 0{
+		return float64(-myf)
+	}
+	return float64(myf)
+}
 func main(){
-	v := vertex{5,7}
-	fmt.Println("vertex(5,7) = ",v.Abs())
+	v1 := vertex1{5,7}
+	fmt.Println("vertex1(5,7) = ",v1.abs())
+
+	v2 := vertex1{3,4}
+	fmt.Println("vertex1(3,4) = ",abs(v2))
+
+	f1 := myfloat(-math.Sqrt2)
+	fmt.Println("f1 = ",f1.abs())
 }
