c4b30c0bb471324c8c98f1295b11942f762e8646
foreverlz1111 <45527640+foreverlz1111@users.noreply.github.com>
2023-02-08 19:40:05 +0800

    更新文件夹 增加图标

--- fi-fileserver/shower/src/requester.js
+++ fi-fileserver/shower/src/requester.js
@@ -2,6 +2,9 @@ import {useCallback, useEffect, useState} from "react";
 
 
 export function GetTableBody() {
+    const dir_icon = (<>&#x1F4C2; </>)
+    const file_icon = (<>&#x1F4C3; </>)
+
     const [dir, setDir] = useState([])
     const getDir = useCallback(async () => {
         const res = await fetch("http://127.0.0.1:3001/g", {
@@ -23,11 +26,14 @@ export function GetTableBody() {
     return (
         <>
             <tbody>
-            {dir.map((v, i) => (
+            {dir && dir.map((v, i) => (
                 <tr key={v.Name}
                     className="border border-slate-300 dark:border-slate-700 p-4 text-slate-500 dark:text-slate-400 hover:bg-gray-200 font-md">
                     <td className="text-slate-900 border">
-                        <button><span>{v.Name}</span></button>
+                        <button>
+                            <span>{v.IsDir ? dir_icon : file_icon}</span>
+                            <span>{v.Name}</span>
+                        </button>
                     </td>
                     <td className="text-slate-900 border">{v.Size}</td>
                     <td className="text-slate-900 border">{v.ModifiedTime}</td>
