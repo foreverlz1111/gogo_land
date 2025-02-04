d31d309ac530c05a8fc02831fe60e68d28a88b79
foreverlz1111 <hi@ziyuan.work>
2025-02-04 14:30:02 +0800

    <feat: 乐扣977>

---
+++ leet/n977.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"log"
+	"sort"
+)
+
+//977. 有序数组的平方
+//给你一个按 非递减顺序 排序的整数数组 nums，返回 每个数字的平方 组成的新数组，要求也按 非递减顺序 排序。
+//
+//
+//
+//示例 1：
+//
+//输入：nums = [-4,-1,0,3,10]
+//输出：[0,1,9,16,100]
+//解释：平方后，数组变为 [16,1,0,9,100]
+//排序后，数组变为 [0,1,9,16,100]
+//示例 2：
+//
+//输入：nums = [-7,-3,2,3,11]
+//输出：[4,9,9,49,121]
+//
+//
+//提示：
+//
+//1 <= nums.length <= 104
+//-104 <= nums[i] <= 104
+//nums 已按 非递减顺序 排序
+
+func sortedSquares(nums []int) []int {
+	new_nums := make([]int, len(nums))
+	for i := 0; i < len(nums); i++ {
+		result := nums[i] * nums[i]
+		new_nums[i] = result
+	}
+	sort.Ints(new_nums)
+	return new_nums
+}
+
+func main() {
+	nums := []int{-4, -1, 0, 3, 10}
+	log.Println(sortedSquares(nums))
+}
