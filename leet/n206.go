c56452ac85b9ec69b63cf5b94505afd5447f1d50
foreverlz1111 <hi@ziyuan.work>
2025-02-12 11:56:26 +0800

    <feat: 乐扣206>

---
+++ leet/n206.go
@@ -0,0 +1,63 @@
+package main
+
+import "log"
+
+//206. 反转链表
+//简单
+//相关标签
+//相关企业
+//给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
+//
+//
+//示例 1：
+//
+//
+//输入：head = [1,2,3,4,5]
+//输出：[5,4,3,2,1]
+//示例 2：
+//
+//
+//输入：head = [1,2]
+//输出：[2,1]
+//示例 3：
+//
+//输入：head = []
+//输出：[]
+//
+//
+//提示：
+//
+//链表中节点的数目范围是 [0, 5000]
+//-5000 <= Node.val <= 5000
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func reverseList(head *ListNode) *ListNode {
+	cur := head.Next
+	var pre *ListNode
+
+	for cur != nil {
+		tmp := cur.Next
+		cur.Next = pre
+		pre = cur
+		cur = tmp
+	}
+	return pre
+}
+func main() {
+	head := &ListNode{0, &ListNode{}}
+	cur := head
+	for i := 1; i < 9; i++ {
+		cur.Next = &ListNode{i, nil}
+		cur = cur.Next
+	}
+
+	cur = reverseList(head)
+	for cur != nil {
+		log.Println(cur)
+		cur = cur.Next
+	}
+}
